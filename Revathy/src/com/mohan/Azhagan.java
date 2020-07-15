369475734f7908a6ebcb539ca082274d037add8d
saravana7378@gmail.com <saravana7378@gmail.com>
2020-07-15 23:03:33 +0530

    gg

---
+++ Revathy/src/com/mohan/Azhagan.java
@@ -0,0 +1,15 @@
+package com.mohan;
+
+public class Azhagan {
+	private static void ff() {
+		int mohan =1;
+		int revathy =1;
+		System.out.println(mohan+revathy+"="+"New Boy Baby");
+
+	}
+	public static void main(String[] args) {
+		ff();
+	}
+	
+
+}
